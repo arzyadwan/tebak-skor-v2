c872b2ced0b99b51d53137e62a8eb37eb5883f5f
unknown <arzyadwan@gmail.com>
2026-07-15 20:24:47 +0700

    migration: add update kickoff time to July 16 at 02:00 WIB

---
+++ supabase/migrations/20260715132500_update_kickoff.sql
@@ -0,0 +1,4 @@
+-- Update kickoff time for England vs Argentina to Thursday, July 16, 2026 at 02:00 AM WIB (local time)
+UPDATE tebak_skor_v2_matches
+SET kickoff_time = '2026-07-16T02:00:00+07:00'
+WHERE id = 1;
